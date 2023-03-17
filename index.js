3ed5bd9042997f086bd7202f51a3401235c2d5bd
unknown <ajitkumarkhatua286@gmail.com>
2023-03-17 11:59:52 +0530

    updated

---
+++ index.js
@@ -0,0 +1,16 @@
+const express=require("express")
+
+const app=express()
+
+app.get("/",(req,res)=>{
+    res.send("Welcome to Landing Page from server")
+})
+
+app.get("/home",(req,res)=>{
+    res.send("Welcome to home page")
+})
+
+
+app.listen(3000,()=>{
+    console.log("server is running at 3000")
+})
